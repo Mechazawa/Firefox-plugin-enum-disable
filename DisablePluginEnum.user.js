ef6d592f2de2a559ead2ac12ae02e257190e393b
j2285441 <j2285441@users.noreply.github.com>
2016-05-03 15:08:58 -0400

    Add script URL

--- DisablePluginEnum.user.js
+++ DisablePluginEnum.user.js
@@ -5,6 +5,8 @@
 // @description Disables plugin enumeration 
 // @license WTFPl 
 // @version 1 
+// @homepageURL https://github.com/Mechazawa/Firefox-plugin-enum-disable
+// @supportURL https://github.com/Mechazawa/Firefox-plugin-enum-disable/issues
 // @include * 
 // @run-at document-start 
 // @updateURL https://raw.githubusercontent.com/Mechazawa/Firefox-plugin-enum-disable/master/DisablePluginEnum.user.js
